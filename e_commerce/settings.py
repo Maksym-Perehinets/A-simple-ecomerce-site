b8a326bf6460c74c81946af26deb78dab5e97b27
Perehinets Maksym <supercomf128@gmail.com>
2024-06-02 16:38:35 +0300

    Project structure refactor and implementation of products form db output

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,6 +128,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "static",
+]
 
 STATIC_URL = 'static/'
 
--- products/views.py
+++ products/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Goods
 
 
 def products_page(request):
-    return render(request, 'products/templates/product-detail.html')
+    desired_product_id = request.GET.get('id')
+    product = get_object_or_404(Goods, pk=desired_product_id)
+    # TODO add comments to db and provide query for retrival
+    # comment =
+    print(product)
+    return render(request,
+                  'products/templates/product-detail.html',
+                  {'product': product,
+                   'comments': 'null'}
+                  )
