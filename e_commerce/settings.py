43af4c9faf71a08bf6350397d5cfb7f8716245fe
Perehinets Maksym <supercomf128@gmail.com>
2024-06-04 19:02:35 +0300

    bug fix

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['webapp-website-prod-eastus.azurewebsites.net']
 
 
 # Application definition
